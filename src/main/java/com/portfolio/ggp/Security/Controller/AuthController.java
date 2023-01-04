676b31fc47f9c8113fb03570da8e4655d1586c99
GinooP <110559505+GinooP@users.noreply.github.com>
2023-01-03 23:50:19 -0300

    vbles DataBases

--- src/main/java/com/portfolio/ggp/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ggp/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8080")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
