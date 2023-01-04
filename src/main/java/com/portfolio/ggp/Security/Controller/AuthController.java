ca1bfff499524e6981a8250ecf15d4d75b1f0d3b
GinooP <110559505+GinooP@users.noreply.github.com>
2023-01-04 11:57:19 -0300

    corrigiendo cors

--- src/main/java/com/portfolio/ggp/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ggp/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "https://frontendggp.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
